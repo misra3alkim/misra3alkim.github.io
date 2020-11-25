0a933b6d12fc92ecb5fb3efeb2255b48663b1360
misra3alkim <74986323+misra3alkim@users.noreply.github.com>
2020-11-25 10:53:37 +0300

    Add files via upload

---
+++ js/typeWriter.js
@@ -0,0 +1,36 @@
+
+let i = 0;
+let text1 = "Mısrakuşum,biricik sevdiceğim iyi ki varsın, seni";
+let text2 = "Çok seviyorum, öpüyorum en güzel yerinden"
+let speed = 100;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
